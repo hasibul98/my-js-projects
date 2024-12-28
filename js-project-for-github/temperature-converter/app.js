1188c7d6980ef046dabe69c099bb56c8bf4b759e
Shakib Ahmed <shakibshovon.10@gmail.com>
2024-12-28 20:13:12 +0600

    fixed an issue

--- js-project-for-github/temperature-converter/app.js
+++ js-project-for-github/temperature-converter/app.js
@@ -1,31 +1,35 @@
-const celsiusEl = document.getElementById('celsius');
-const fahrenheitEl = document.getElementById('fahrenheit');
-const kelvinEl = document.getElementById('kelvin');
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
 
-function computeTemp(event){
-       const currentValue = +event.target.value;
+function computeTemp(event) {
+  const currentValue = parseFloat(event.target.value);
 
-       switch(event.target.name){
-              case 'celsius':
-                     kelvinEl.value = (currentValue + 273.15).toFixed(2);
-                     fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
-                     break;
-                   case 'fahrenheit':
-                     celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
-                     kelvinEl.value = (((currentValue - 32) / 1.8) + 273.15).toFixed(2);
-                     break;
-                   case 'kelvin':
-                     celsiusEl.value = (currentValue - 273.15).toFixed(2);
-                     fahrenheitEl.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
-                     break;
-              default:
-                     break;  
-                    
+  if (currentValue !== "") {
+    switch (event.target.name) {
+      case "celsius":
+        kelvinEl.value = (currentValue + 273.15).toFixed(2);
+        fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+        break;
+      case "fahrenheit":
+        celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+        kelvinEl.value = ((currentValue - 32) / 1.8 + 273.15).toFixed(2);
+        break;
+      case "kelvin":
+        celsiusEl.value = (currentValue - 273.15).toFixed(2);
+        fahrenheitEl.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
+        break;
+      default:
+        break;
+    }
+  } else {
+    celsiusEl.value = "";
+    fahrenheitEl.value = "";
+    kelvinEl.value = "";
+  }
 
-       }
-       console.log(celsiusEl.value,fahrenheitEl.value,kelvinEl.value);
+  console.log(celsiusEl.value, fahrenheitEl.value, kelvinEl.value);
 }
-fahrenheitEl.addEventListener('input', computeTemp);
-celsiusEl.addEventListener('input', computeTemp);
-kelvinEl.addEventListener('input', computeTemp);
-
+fahrenheitEl.addEventListener("input", computeTemp);
+celsiusEl.addEventListener("input", computeTemp);
+kelvinEl.addEventListener("input", computeTemp);
